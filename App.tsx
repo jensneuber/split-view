363107eb9dea6bbb7e36a4320a443ebe02202322
Jens Neuber <mail@jensneuber.de>
2022-01-18 11:40:19 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
